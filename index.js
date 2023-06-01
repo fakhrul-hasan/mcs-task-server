7bf6c519b5187c4d51fbc4a8cf0453037f34615b
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-06-02 00:30:46 +0600

    delete from db api worked

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xyrtm8p.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,6 +34,12 @@ async function run() {
     app.get('/tasks', async(req,res)=>{
         const result = await tasksCollection.find().toArray();
         res.send(result);
+    })
+    app.delete('/tasks/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await tasksCollection.deleteOne(query);
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
